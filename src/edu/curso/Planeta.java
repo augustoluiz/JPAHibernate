46b48ee60bbed32b4ae1d3744d248d7363c6346a
augustoluiz <augustogu7@yahoo.com.br>
2019-04-17 21:25:09 -0300

    JPA com Hibernate

---
+++ src/edu/curso/Planeta.java
@@ -0,0 +1,62 @@
+package edu.curso;
+
+import java.io.Serializable;
+import java.util.Date;
+
+//sempre importar do javax.persistence
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Planeta implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private long id;
+	private String nome;
+	private float distanciaSol;
+	private double tamanho;
+	private int qtdLuas;
+	private Date descoberto;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getDistanciaSol() {
+		return distanciaSol;
+	}
+	public void setDistanciaSol(float distanciaSol) {
+		this.distanciaSol = distanciaSol;
+	}
+	public double getTamanho() {
+		return tamanho;
+	}
+	public void setTamanho(double tamanho) {
+		this.tamanho = tamanho;
+	}
+	public int getQtdLuas() {
+		return qtdLuas;
+	}
+	public void setQtdLuas(int qtdLuas) {
+		this.qtdLuas = qtdLuas;
+	}
+	public Date getDescoberto() {
+		return descoberto;
+	}
+	public void setDescoberto(Date descoberto) {
+		this.descoberto = descoberto;
+	}
+}
